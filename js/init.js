868bbfc13f41b563dadf27261ea3ef6a4d6d386e
Caleb <calebbelcourt@hotmail.com>
2021-01-21 12:38:36 -0800

    Added Projects to Portfolio
    Added projects to portfolio. Made some other minor verbiage changes and photo changes.

--- js/init.js
+++ js/init.js
@@ -139,7 +139,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 
 		animateSpan.typed({
-			strings: ["Game Developer", "Web Developer", "Mobile App Developer"],
+			strings: ["Web Developer", "Game Developer", "Mobile App Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
